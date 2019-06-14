f3418d9050e334d04ef9285e9e839f6a4795bfa6
DICKY IBROHIM <dicky.director@gmail.com>
2019-06-15 02:36:40 +0700

    First Upload
    First Upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// DICKY IBROHIM
+int main()
+//MEMINDAH NILAI A KE B
+{ int a,b,c;
+a=10; b=90;
+    printf("a=%d,b=%d\n",a,b);
+c=a; // C Diisi dengan Nilai A
+a=b; // A Diisi dengan Nilai B
+b=c; // B Diisi dengan Nilai C
+printf("HASIL a=%d,b=%d",a,b);
+    return 0;
+}
